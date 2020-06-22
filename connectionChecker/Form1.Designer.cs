a668bda3692b1c9e5b82bfd0c0e1b87e536055b1
MADMechaniculus <rihanna271298@gmail.com>
2020-06-22 07:06:22 +0300

    Initial commit

---
+++ connectionChecker/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace connectionChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusText = new System.Windows.Forms.Label();
+            this.URIText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonStart.Location = new System.Drawing.Point(9, 9);
+            this.buttonStart.Margin = new System.Windows.Forms.Padding(0);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(250, 23);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Запуск";
+            this.buttonStart.UseVisualStyleBackColor = false;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonStop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonStop.Location = new System.Drawing.Point(9, 35);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(250, 23);
+            this.buttonStop.TabIndex = 1;
+            this.buttonStop.Text = "Остановить";
+            this.buttonStop.UseVisualStyleBackColor = false;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Status:";
+            // 
+            // statusText
+            // 
+            this.statusText.AutoSize = true;
+            this.statusText.Location = new System.Drawing.Point(52, 61);
+            this.statusText.Name = "statusText";
+            this.statusText.Size = new System.Drawing.Size(58, 13);
+            this.statusText.TabIndex = 3;
+            this.statusText.Text = "STOPPED";
+            // 
+            // URIText
+            // 
+            this.URIText.Location = new System.Drawing.Point(9, 97);
+            this.URIText.Name = "URIText";
+            this.URIText.Size = new System.Drawing.Size(248, 20);
+            this.URIText.TabIndex = 4;
+            this.URIText.Text = "https://www.microsoft.com/";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Enter connectable URI";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(221, 81);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(36, 13);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "LOGS";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(269, 127);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.URIText);
+            this.Controls.Add(this.statusText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Connection Checker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label statusText;
+        private System.Windows.Forms.TextBox URIText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ connectionChecker/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+using System.Net;
+using System.Media;
+using System.Diagnostics;
+
+namespace connectionChecker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        String soundDir = "sounds";
+        String alaram = @"sounds\ta_da.wav";
+
+        String reportsDir = "reports";
+        String activeReport = null;
+
+        String activeURI = null;
+
+        StreamWriter reportWrite;
+
+        public delegate void connectionOkDel();
+        public delegate void connectionErrDel();
+        public delegate void enButton();
+
+        connectionOkDel conOk;
+        connectionErrDel conErr;
+        enButton enBtn;
+
+        SoundPlayer player = null;
+
+        bool isStopped = false;
+
+        public void ConnectionOK()
+        {
+            this.statusText.Invoke((MethodInvoker)delegate
+            {
+                this.statusText.Text = "CONNECTION NORMAL";
+            });
+        }
+
+        public void ConnectionERR()
+        {
+            if (player != null)
+            {
+                player.Play();
+            }
+            this.statusText.Invoke((MethodInvoker)delegate
+            {
+                this.statusText.Text = "CONNECTION ERROR";
+            });
+        }
+
+        public void enableStartButton()
+        {
+            this.buttonStart.Invoke((MethodInvoker)delegate
+            {
+                this.buttonStart.Enabled = true;
+            });
+            this.statusText.Invoke((MethodInvoker)delegate
+            {
+                this.statusText.Text = "STOPPED";
+            });
+        }
+
+        public void checkConnection()
+        {
+            while (true)
+            {
+                if (isStopped)
+                { 
+                    enBtn.Invoke();
+                    isStopped = false;
+                    return;
+                }
+
+                Thread.Sleep(500);
+
+                HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(activeURI);
+                try
+                {
+                    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                    Stream temp = response.GetResponseStream();
+                    StreamReader sr = new StreamReader(temp);
+                    sr.ReadToEnd();
+                    //connection OK
+                    conOk.Invoke();
+                }
+                catch (WebException ex)
+                {
+                    //Conncetion ERR
+                    String msg = ex.Message;
+                    conErr.Invoke();
+                    reportWrite.WriteLine("ОТКАЗ::" + DateTime.Now.ToString());
+                }
+            }
+        }
+
+        Thread check = null;
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            activeURI = URIText.Text;
+            String date = DateTime.Now.ToString();
+
+            date = date.Replace(":", "_");
+
+            activeReport = date + "_report.txt";
+
+            reportWrite = new StreamWriter(reportsDir+@"\"+ activeReport);
+
+            check = new Thread(checkConnection);
+
+            check.Start();
+
+            buttonStart.Enabled = false;
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            isStopped = true;
+            if (reportWrite != null)
+                reportWrite.Close();
+            if (isStopped == false)
+            {
+                check.Abort();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conOk = ConnectionOK;
+            conErr = ConnectionERR;
+            enBtn = enableStartButton;
+
+            if (Directory.Exists(soundDir))
+            {
+                player = new SoundPlayer(alaram);
+            }
+            else
+            {
+                MessageBox.Show("Папка со звуками не найдена!", "АЛЯРМА!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Directory.CreateDirectory(soundDir);
+            }
+
+            if (!Directory.Exists(reportsDir))
+            {
+                Directory.CreateDirectory(reportsDir);
+            }
+
+            check = new Thread(checkConnection);
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(reportsDir);
+        }
+    }
+}
